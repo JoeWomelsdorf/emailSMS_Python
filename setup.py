61fb8b3b108bc61f03ea7c0578873b11b7c9efbf
JoeWomelsdorf <62812191+JoeWomelsdorf@users.noreply.github.com>
2022-02-19 14:38:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import setup
+import setuptools
 from EmailSMS import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='emailSMS-python',
     version=1.0,
 
     url='https://github.com/JoeWomelsdorf/Python_Email2SMS',
     author='Joe Womelsdorf',
     author_email='josef.womelsdorf@outlook.com',
+    packages=setuptools.find_packages(),
     description="An email to sms gateway for Python3",
+         classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+
 
     py_modules=['my_pip_package'],
 )
